bd95e14de60489b541ed86f6d7c7a4c9711f6188
mostafij <mostafijul1000@gmail.com>
2023-08-05 02:49:23 +0600

    auto generated files added!

---
+++ todo_client/lib/src/constants/server/api/api_config.g.dart
@@ -0,0 +1,189 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'api_config.dart';
+
+// **************************************************************************
+// EnviedGenerator
+// **************************************************************************
+
+class _APIConfig {
+  static const List<int> _enviedkeybaseURl = [
+    3849029926,
+    252921031,
+    66431800,
+    4249739154,
+    2559644514,
+    1593906782,
+    2550453276,
+    3621509399,
+    2098112495,
+    3922041304,
+    1736670095,
+    3116364155,
+    3892124546,
+    4187688675,
+    938445525,
+    1982100828,
+    3726912152,
+    1017887011,
+    1675805126,
+    769828007,
+    1802044981
+  ];
+  static const List<int> _envieddatabaseURl = [
+    3849029966,
+    252921011,
+    66431820,
+    4249739234,
+    2559644504,
+    1593906801,
+    2550453299,
+    3621509499,
+    2098112384,
+    3922041275,
+    1736670190,
+    3116364055,
+    3892124650,
+    4187688588,
+    938445478,
+    1982100776,
+    3726912162,
+    1017887003,
+    1675805174,
+    769827999,
+    1802044933
+  ];
+  static final String baseURl = String.fromCharCodes(
+    List.generate(_envieddatabaseURl.length, (i) => i, growable: false)
+        .map((i) => _envieddatabaseURl[i] ^ _enviedkeybaseURl[i])
+        .toList(growable: false),
+  );
+  static const List<int> _enviedkeyaddTodo = [
+    2359335017,
+    2063911913,
+    4259256628,
+    2889355676,
+    112835106,
+    4217954047,
+    4188290169,
+    2938391541,
+    627380264,
+    3877862406
+  ];
+  static const List<int> _envieddataaddTodo = [
+    2359334982,
+    2063911837,
+    4259256667,
+    2889355768,
+    112835149,
+    4217953932,
+    4188290134,
+    2938391444,
+    627380300,
+    3877862498
+  ];
+  static final String addTodo = String.fromCharCodes(
+    List.generate(_envieddataaddTodo.length, (i) => i, growable: false)
+        .map((i) => _envieddataaddTodo[i] ^ _enviedkeyaddTodo[i])
+        .toList(growable: false),
+  );
+  static const List<int> _enviedkeyallTodo = [
+    1561286710,
+    1708642827,
+    1650986859,
+    1869487913,
+    139380765,
+    1447282297,
+    1630530746,
+    2271124261,
+    1315401330,
+    412545314
+  ];
+  static const List<int> _envieddataallTodo = [
+    1561286681,
+    1708642943,
+    1650986756,
+    1869487949,
+    139380850,
+    1447282186,
+    1630530709,
+    2271124292,
+    1315401246,
+    412545358
+  ];
+  static final String allTodo = String.fromCharCodes(
+    List.generate(_envieddataallTodo.length, (i) => i, growable: false)
+        .map((i) => _envieddataallTodo[i] ^ _enviedkeyallTodo[i])
+        .toList(growable: false),
+  );
+  static const List<int> _enviedkeyupdateTodo = [
+    2319001683,
+    1392600144,
+    3387766203,
+    3776057784,
+    1960179931,
+    2398474240,
+    2045364862,
+    474248921,
+    2425580223,
+    3244711685,
+    1873533472,
+    167048691,
+    2039410027
+  ];
+  static const List<int> _envieddataupdateTodo = [
+    2319001724,
+    1392600100,
+    3387766228,
+    3776057820,
+    1960179892,
+    2398474355,
+    2045364817,
+    474248876,
+    2425580239,
+    3244711777,
+    1873533505,
+    167048583,
+    2039409934
+  ];
+  static final String updateTodo = String.fromCharCodes(
+    List.generate(_envieddataupdateTodo.length, (i) => i, growable: false)
+        .map((i) => _envieddataupdateTodo[i] ^ _enviedkeyupdateTodo[i])
+        .toList(growable: false),
+  );
+  static const List<int> _enviedkeydeleteTodo = [
+    3146387467,
+    2370673465,
+    847686296,
+    1055103705,
+    803593521,
+    1633034342,
+    2146277128,
+    2234776276,
+    1148015897,
+    2057265491,
+    2395273892,
+    710685085,
+    1164802879
+  ];
+  static const List<int> _envieddatadeleteTodo = [
+    3146387492,
+    2370673485,
+    847686391,
+    1055103677,
+    803593566,
+    1633034261,
+    2146277159,
+    2234776240,
+    1148015996,
+    2057265471,
+    2395273921,
+    710685161,
+    1164802906
+  ];
+  static final String deleteTodo = String.fromCharCodes(
+    List.generate(_envieddatadeleteTodo.length, (i) => i, growable: false)
+        .map((i) => _envieddatadeleteTodo[i] ^ _enviedkeydeleteTodo[i])
+        .toList(growable: false),
+  );
+}
