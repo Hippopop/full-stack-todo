8bc1d6a3b24eab483d1724109323b103bb7330d5
Mostafijul Islam <mostafijul1000@gmail.com>
2024-08-14 04:44:29 +0600

    cleanup: dist directory removed!

--- todo_server/dist/routes/token/errors/refresh_token_errors.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.noRefreshTokenError = exports.faultyRefreshToken = void 0;
-const error_codes_1 = require("../../../errors/error_codes");
-const error_z_1 = require("../../../types/response/errors/error-z");
-exports.faultyRefreshToken = new error_z_1.ResponseError(error_codes_1.unauthorized, "Provided refresh token was faulty!");
-exports.noRefreshTokenError = new error_z_1.ResponseError(error_codes_1.badRequest, "Please provide the refresh token!");
-//# sourceMappingURL=refresh_token_errors.js.map
