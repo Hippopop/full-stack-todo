04d5e52343f37f51cfd6d47f32ea87aa7e3b5fb3
Mostafijul Islam <mostafijul1000@gmail.com>
2024-08-14 15:06:36 +0600

    cleanup: fixed the missing files!

---
+++ todo_server/src/errors/custom_error.ts
@@ -0,0 +1,12 @@
+export abstract class CustomErrorStructure extends Error {
+
+  abstract status: number;
+  abstract code?: number | string | Array<string> | Array<number>;
+
+  constructor(message: string) {
+    super(message);
+    Object.setPrototypeOf(this, CustomErrorStructure.prototype);
+  }
+
+  abstract serializeErrors(): any;
+}
